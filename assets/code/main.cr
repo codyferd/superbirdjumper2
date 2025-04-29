18cdd7d433a311065a3befa28ee0f045b7110b63
codyferd <lucarappferdinand@gmail.com>
2025-04-29 16:44:05 -0400

    Made the texture show, and fixed variables

--- assets/code/main.cr
+++ assets/code/main.cr
@@ -2,23 +2,25 @@ require "chipmunk"
 require "raylib-cr"
 
 # Window & Physics Constants
-W = 1920
-H = 1000
+W = Raylib.get_screen_width()
+H = Raylib.get_screen_height()
 JUMP_FORCE   = CP::Vect.new(0, 300.0)
-GRAVITY      = CP::Vect.new(0, -980.0)  # pixels/sec²
+GRAVITY      = CP::Vect.new(0, 400.0)  # pixels/sec²
 BIRD_RADIUS  = 20.0
 
 # Init Raylib window
 Raylib.init_window(W, H, "Chipmunk + Raylib-cr")
 Raylib.set_target_fps(50)
+Raylib.toggle_fullscreen()
 
 # Set up Chipmunk space
 space = CP::Space.new
 space.gravity = GRAVITY
 
 # Bird (dynamic circle)
+bird_texture = Raylib.load_texture("assets/images/bird.png")
 bird_body  = CP::Body.new(1.0, 1.0)
-bird_body.position = CP::Vect.new(W/4, H/2)
+bird_body.position = CP::Vect.new(500, H * 0.5)
 bird_shape = CP::Shape::Circle.new(bird_body, BIRD_RADIUS, CP::Vect.new(0, 0))
 bird_shape.friction   = 0.7
 bird_shape.elasticity = 0.5
@@ -29,12 +31,11 @@ until Raylib.close_window?
   # Step physics
   space.step(1.0/50.0)
 
-  key = Raylib.get_key_pressed
+  key = Raylib.get_key_pressed()
 
   if key == Raylib::KeyboardKey::W ||
     key == Raylib::KeyboardKey::A ||
     key == Raylib::KeyboardKey::D
-  
     bird_body.apply_impulse_at_world_point(JUMP_FORCE, bird_body.position)
   end
   
@@ -42,19 +43,26 @@ until Raylib.close_window?
   if key == Raylib::KeyboardKey::Q || key == Raylib::KeyboardKey::Escape
     break
   end
+  
+  if bird_body.position == 0 || bird_body.position == H
+    break
+  end
+  
+  # Get and display FPS
+  fps = Raylib.get_fps()
+  Raylib.draw_text("#{fps} FPS", 1800, 10, 20, Raylib::BLACK)
 
   # Draw
   Raylib.begin_drawing
   Raylib.clear_background(Raylib::SKYBLUE)
 
   # Draw bird
-  Raylib.draw_circle(
-    bird_body.position.x.to_i,
-    bird_body.position.y.to_i,
-    BIRD_RADIUS,
-    Raylib::RED
+  Raylib.draw_texture(
+    bird_texture,
+    bird_body.position.x.to_i - bird_texture.width // 2,
+    bird_body.position.y.to_i - bird_texture.height // 2,
+    Raylib::WHITE
   )
-
   Raylib.end_drawing
 end
 
