c94d1f32875326f0090e6a4708a2c7f312d9c867
codyferd <lucarappferdinand@gmail.com>
2025-05-06 19:09:40 -0400

    Added game over

--- assets/code/main.cr
+++ assets/code/main.cr
@@ -8,7 +8,7 @@ H = 1080
 JUMP_FORCE   = CP::Vect.new(0, -250.0)
 GRAVITY      = CP::Vect.new(0, 250.0)
 BIRD_RADIUS  = 20.0
-game_over    = 0
+game_over    = false
 
 # AppImage stuff
 if Dir.current == "/home/luca/superbirdjumper2"
@@ -41,13 +41,27 @@ backround_texture = LibRay.load_texture("#{asset_path}/assets/images/backround.j
 
 # Main Loop
 until LibRay.window_should_close?
+  LibRay.begin_drawing
+  LibRay.draw_texture(backround_texture, 0, 0, LibRay::SKYBLUE)
+  if game_over
+    LibRay.draw_text("GAME OVER", W//2 - 200, H//2 - 40, 60, LibRay::RED)
+    LibRay.draw_text("Press W to Restart", W//2 - 200, H//2 + 30, 30, LibRay::BLACK)
+    key = LibRay.get_key_pressed()
+    if key == 87
+      bird_body.position = CP::Vect.new(W * 0.25, H * 0.5)
+      game_over = false
+    end
+    LibRay.end_drawing
+    next
+  end
+
   # Step physics
   space.step(1.0/50.0)
 
   key = LibRay.get_key_pressed()
 
   if key == 87 || key == 65 || key == 68
-      bird_body.apply_impulse_at_world_point(JUMP_FORCE, bird_body.position)
+    bird_body.apply_impulse_at_world_point(JUMP_FORCE, bird_body.position)
   end
   
   if key == 83
@@ -55,22 +69,14 @@ until LibRay.window_should_close?
   end
 
   if bird_body.position.y >= H || bird_body.position.y <= 0
-    Process.exec(Process.executable_path.not_nil!)
+    game_over = true
   end
-  
-#  if key == 69
-#    # Inventory/shop
-#  end
-#  if key == 81
-#   # Use power-up
-#  end
 
   # Get and display FPS
   fps = LibRay.get_fps()
   LibRay.draw_text("#{fps} FPS", 1800, 10, 20, LibRay::BLACK)
 
-  # Draw
-  LibRay.begin_drawing
+  # Draw background
   LibRay.draw_texture(backround_texture, 0, 0, LibRay::SKYBLUE)
 
   # Draw bird
